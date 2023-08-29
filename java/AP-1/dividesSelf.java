4b2657adeb5dac9406c4303d10277cd5237456e4
Art Vandelay <mnsenna@protonmail.com>
2023-08-30 09:31:30 +1000

    Complete dividesSelf

---
+++ java/AP-1/dividesSelf.java
@@ -0,0 +1,18 @@
+// We'll say that a positive int divides itself if every digit in the number divides into the number evenly. So for example 128 divides itself since 1, 2, and 8 all divide into 128 evenly. We'll say that 0 does not divide into anything evenly, so no number with a 0 digit divides itself. Note: use % to get the rightmost digit, and / to discard the rightmost digit.
+
+// dividesSelf(128) → true
+// dividesSelf(12) → true
+// dividesSelf(120) → false
+
+public boolean dividesSelf(int n) {
+  boolean result = true;
+  
+  for (int total = n; total != 0; total /= 10 ) {
+    int lastDigit = total % 10;
+    if (lastDigit == 0 || n % lastDigit != 0) {
+      result = false;
+    }
+  }
+
+  return result;
+}
